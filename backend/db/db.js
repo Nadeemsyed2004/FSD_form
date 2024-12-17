0c81124a7f21e82673f6fb9eddf2c11bb401b157
Syed <syednadeem192004@gmail.com>
2024-12-17 23:53:19 +0530

    Updated backend configuration and database connection

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,12 +1,13 @@
 const mysql = require('mysql2/promise');
+require('dotenv').config();
 
 const connection = async()=>{
     try{
         const connect = await mysql.createConnection({
-            host:'localhost',
-            user:'root',
-            password:'Nadeem@2004',
-            database:'employee_table'
+            host: process.env.DB_HOST || 'localhost',
+            user:process.env.DB_USER || 'root',
+            password: process.env.DB_PASS || 'Nadeem@2004',
+            database:process.env.DB_NAME || 'employee_details'
         });
         console.log("Databse Connected Successfully!");
         return connect;
