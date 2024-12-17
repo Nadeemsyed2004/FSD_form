f2238a24d6acb3f34d02743a1fca1ab5f81ce463
Nadeem Syed <132538079+Nadeemsyed2004@users.noreply.github.com>
2024-12-17 13:40:15 +0530

    Update EmployeeForm.js

--- frontend/src/components/EmployeeForm.js
+++ frontend/src/components/EmployeeForm.js
@@ -9,7 +9,7 @@ function EmployeeForm() {
     const handleSubmit = async (values, { resetForm }) => {
         try {
           const response = await fetch(
-            "http://localhost:5000/api/auth/addEmployee",
+            "https://fsd-form-f9tt.onrender.com/api/auth/addEmployee",
             {
               method: "POST",
               headers: { "Content-Type": "application/json" },
